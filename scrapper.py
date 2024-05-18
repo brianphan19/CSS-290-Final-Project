9663bb0b4bade7af3f23255fcb0b5296ebeeb69c
brianphan19 <brianphanvinh@gmail.com>
2024-05-17 18:25:15 -0700

    added ability to get temperature

--- scrapper.py
+++ scrapper.py
@@ -21,6 +21,24 @@ class Scrapper():
 
         return unit_value, unit_label
     
+    def get_temperature(self, loc_lat: float, loc_lon: float):
+        url: str = f'https://globalsolaratlas.info/map?c=11.609193,8.261719,3&s={loc_lat},{loc_lon}&m=site'
+        self.driver.get(url)
+        self.driver.implicitly_wait(10)
+
+        temperature_value = self.driver.find_element(By.XPATH, "//div[contains(@class, 'site-data__layer-name') and contains(., 'TEMP')]/following-sibling::div[contains(@class, 'site-data__unit-value')]//sg-unit-value-inner").text.strip()
+        return temperature_value
+    
+    def get_values(self, loc_lat: float, loc_lon: float):
+        url: str = f'https://globalsolaratlas.info/map?c=11.609193,8.261719,3&s={loc_lat},{loc_lon}&m=site'
+        self.driver.get(url)
+        self.driver.implicitly_wait(10)
+        
+        unit_value = self.driver.find_element(By.CSS_SELECTOR, '.site-data__unit-value sg-unit-value-inner').text.strip()
+        temperature_value = self.driver.find_element(By.XPATH, "//div[contains(@class, 'site-data__layer-name') and contains(., 'TEMP')]/following-sibling::div[contains(@class, 'site-data__unit-value')]//sg-unit-value-inner").text.strip()
+        
+        return unit_value, temperature_value
+    
     def close(self):
         self.driver.quit()
 
@@ -38,9 +56,10 @@ def main() -> None:
     for latitude, longitude in coordinates:
         try:
             unit_value, unit_label = scrap.get_unit_value(latitude, longitude)
-            results.append((latitude, longitude, unit_value, unit_label))
+            temperature_value, temperature_label = scrap.get_temperature(latitude, longitude)
+            results.append((latitude, longitude, unit_value, unit_label, temperature_value, temperature_label))
         except Exception as e:
-            results.append((latitude, longitude, 'Error', str(e)))
+            results.append((latitude, longitude, 'Error', str(e), 'Error', str(e)))
 
     scrap.close()
 
