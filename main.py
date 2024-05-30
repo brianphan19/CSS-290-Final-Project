1ae43dd97f2bff3aa25626f303106e44fb0c7e9a
brianphan19 <brianphanvinh@gmail.com>
2024-05-30 15:57:49 -0700

    try 500 farm with 20 max thread

--- main.py
+++ main.py
@@ -14,29 +14,37 @@ df['p_cap_tot'] = df['p_cap_dc'] + df['p_cap_ac']
 df['avg_area'] = df['p_cap_tot'] / df['p_area']
 
 # Function to fetch data using the Scrapper class
-def fetch_data(row,  results, index):
-    scrapper = Scrapper()
-    
-    latitude = row['ylat']
-    longitude = row['xlong']
+def fetch_data(row, results, index, semaphore):
+    semaphore.acquire()  # Acquire semaphore to limit the number of threads
+    try:
+        scrapper = Scrapper()
+        
+        latitude = row['ylat']
+        longitude = row['xlong']
 
-    unit_value = scrapper.get_unit_value(latitude, longitude)
-    scrapper.close()
-    numeric_value = int(float(re.findall(r"[-+]?\d*\.\d+|\d+", unit_value)[0]))
-    results[index] = (row['p_name'], row['p_area'], row['p_cap_ac'], row['p_cap_dc'], numeric_value)
+        unit_value = scrapper.get_unit_value(latitude, longitude)
+        scrapper.close()
+        numeric_value = int(float(re.findall(r"[-+]?\d*\.\d+|\d+", unit_value)[0]))
+        results[index] = (row['p_name'], row['p_area'], row['p_cap_ac'], row['p_cap_dc'], numeric_value)
+    finally:
+        semaphore.release()  # Release semaphore after scraping is done
 
-# Select the top 10 rows with the highest z-scores
-d = df.nlargest(10, 'p_zscore')
-print(d)
+# Select the top 100 rows with the highest z-scores
+d = df.nlargest(500, 'p_zscore')
 
 # Prepare a list to store the results and threads
 results = [None] * len(d)
 threads = []
 index = 0
+
+# Create a semaphore with a maximum of 10 permits (allowing 10 tabs to be opened at a time)
+max_threads = 20
+semaphore = threading.Semaphore(max_threads)
+
 # Iterate over the selected rows
 for i, row in d.iterrows():
     # Start a new thread for each row
-    thread = threading.Thread(target=fetch_data, args=(row, results, index))
+    thread = threading.Thread(target=fetch_data, args=(row, results, index, semaphore))
     threads.append(thread)
     thread.start()
     index += 1
@@ -69,6 +77,5 @@ for name, color in color_map.items():
 plt.title('Area vs Unit Value')
 plt.xlabel('Area (m^2)')
 plt.ylabel('Unit Value (kWh/kWp)')
-plt.legend(title='Plant Names')
 plt.grid(True)
 plt.show()
